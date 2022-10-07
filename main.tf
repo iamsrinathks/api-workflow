aa1762f665e9f5d8b2a714498a990ffaaefbb98d
iamsrinathks <104845233+iamsrinathks@users.noreply.github.com>
2022-10-07 19:59:44 +0100

    Adding gcs bucket module

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
-module "bucket" {
-  source  = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
-  version = "~> 1.3"
-
-  name       = "example-bucket"
-  project_id = "example-project"
-  location   = "us-east1"
+module "gcs_buckets" {
+  source  = "terraform-google-modules/cloud-storage/google"
+  project_id  = var.project_id
+  names = var.name
+  prefix = var.prefix
 }
